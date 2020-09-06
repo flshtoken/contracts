6113f9ee4c7e1f76d6b9ae6ef9168ee7194c2f18
flshtoken <70830559+flshtoken@users.noreply.github.com>
2020-09-05 23:49:55 -0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,35 +1,92 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/GSN/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Capped.sol";
 
-contract Token is ERC20 {
-  using SafeMath for uint;
-  address public admin;
-  uint public maxTotalSupply;
+/**
+ * @dev {ERC20} token, including:
+ *
+ *  - ability for holders to burn (destroy) their tokens
+ *  - a minter role that allows for token minting (creation)
+ *  - a pauser role that allows to stop all token transfers
+ *
+ * This contract uses {AccessControl} to lock permissioned functions using the
+ * different roles - head to its documentation for details.
+ *
+ * The account that deploys the contract will be granted the minter and pauser
+ * roles, as well as the default admin role, which will let it grant both minter
+ * and pauser roles to other accounts.
+ */
+contract Token is Context, AccessControl, ERC20Burnable, ERC20Pausable, ERC20Capped(4200000000) {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-  constructor(
-    string memory name, 
-    string memory symbol,
-    uint _maxTotalSupply
-  ) public ERC20("Nacho Finance", "NACHOS") {
-    admin = msg.sender;
-    maxTotalSupply = _maxTotalSupply;
-  }
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+     * account that deploys the contract.
+     *
+     * See {ERC20-constructor}.
+     */
+    constructor(string memory name, string memory symbol) public ERC20("Nacho Finance", "NACHOS") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
-  function updateAdmin(address newAdmin) public {
-    require(msg.sender == admin, 'only admin');
-    admin = newAdmin;
-  }
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+        _mint(msg.sender,70000000);
+        _setupDecimals(2);
+    }
 
-  function mint(address account, uint256 amount) public {
-    require(msg.sender == admin, 'only admin');
-    uint totalSupply = totalSupply();
-    require(
-      totalSupply.add(amount) <= maxTotalSupply, 
-      'above maxTotalSupply limit'
-    );
-    _mint(msg.sender,700000000);
-    _setupDecimals(2);
-  }
+    /**
+     * @dev Creates `amount`  tokens for `to`.
+     *new
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to pause");
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_unpause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have pauser role to unpause");
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable, ERC20Capped) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
